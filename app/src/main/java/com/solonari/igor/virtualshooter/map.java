e36458e98fd14eaed4924df2908a31eb948686d3
igorriok <i.solonari@gmail.com>
2016-05-17 10:32:07 +0300

    Update map.java

--- app/src/main/java/com/solonari/igor/virtualshooter/map.java
+++ app/src/main/java/com/solonari/igor/virtualshooter/map.java
@@ -50,7 +50,37 @@ public abstract class map extends AppCompatActivity implements
                 .addApi(LocationServices.API)
                 .build();
         }
+        
+        updateValuesFromBundle(savedInstanceState);
     }
+
+        private void updateValuesFromBundle(Bundle savedInstanceState) {
+            if (savedInstanceState != null) {
+                // Update the value of mRequestingLocationUpdates from the Bundle, and
+                // make sure that the Start Updates and Stop Updates buttons are
+                // correctly enabled or disabled.
+                if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+                    mRequestingLocationUpdates = savedInstanceState.getBoolean(
+                            REQUESTING_LOCATION_UPDATES_KEY);
+                    setButtonsEnabledState();
+                }
+        
+                // Update the value of mCurrentLocation from the Bundle and update the
+                // UI to show the correct latitude and longitude.
+                if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+                    // Since LOCATION_KEY was found in the Bundle, we can be sure that
+                    // mCurrentLocationis not null.
+                    mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+                }
+        
+                // Update the value of mLastUpdateTime from the Bundle and update the UI.
+                if (savedInstanceState.keySet().contains(LAST_UPDATED_TIME_STRING_KEY)) {
+                    mLastUpdateTime = savedInstanceState.getString(
+                            LAST_UPDATED_TIME_STRING_KEY);
+                }
+                updateUI();
+            }
+        }
 
         protected void onStart() {
             mGoogleApiClient.connect();
@@ -131,7 +161,13 @@ public abstract class map extends AppCompatActivity implements
 
     }
 
-
+        public void onSaveInstanceState(Bundle savedInstanceState) {
+            savedInstanceState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+                    mRequestingLocationUpdates);
+            savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);
+            savedInstanceState.putString(LAST_UPDATED_TIME_STRING_KEY, mLastUpdateTime);
+            super.onSaveInstanceState(savedInstanceState);
+        }
 
 }
 
