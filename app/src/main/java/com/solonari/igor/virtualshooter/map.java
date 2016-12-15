d2e1dcdcd27d8aae92cbd6fb2b162e19f2ee6aa9
igor <i.solonari@gmail.com>
2016-12-15 21:31:36 +0200

    begin

--- app/src/main/java/com/solonari/igor/virtualshooter/map.java
+++ app/src/main/java/com/solonari/igor/virtualshooter/map.java
@@ -266,5 +266,15 @@ public class map extends AppCompatActivity implements
         }
     }
 
+    // Find the View that shows the numbers category
+    TextView numbers = (TextView) findViewById(R.id.numbers);
+    // Set a click listener on that View
+    numbers.setOnClickListener(new View.OnClickListener() {
+        // The code in this method will be executed when the numbers View is clicked on.
+        @Override
+        public void onClick(View view) {
+        Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+        startActivity(numbersIntent); } });
+
 }
 
