1f26a00976b354581f83d9d18accd2a60970a21d
igorriok <i.solonari@gmail.com>
2017-02-01 11:07:08 +0200

    clean up

--- app/src/main/java/com/solonari/igor/virtualshooter/TCPClient.java
+++ app/src/main/java/com/solonari/igor/virtualshooter/TCPClient.java
@@ -26,7 +26,6 @@ public class TCPClient {
     private MessageCallback listener = null;
     private boolean mRun = false;
 
-
     /**
      * TCPClient class constructor, which is created in AsyncTasks after the button click.
      *
@@ -42,9 +41,7 @@ public class TCPClient {
         this.mHandler = mHandler;
     }
 
-    /**
-     * Public method for sending the message via OutputStream object.
-     *
+    /**Public method for sending the message via OutputStream object.
      * @param message Message passed as an argument and sent via OutputStream object.
      */
     public void sendMessage(String message) {
@@ -66,73 +63,47 @@ public class TCPClient {
     }
 
     public void run() {
-
         mRun = true;
-
         try {
             // Creating InetAddress object from ipNumber passed via constructor from IpGetter class.
             InetAddress serverAddress = InetAddress.getByName(ipNumber);
-
             Log.d(TAG, "Connecting...");
-
-            /**
-             * Sending empty message with static int value from MainActivity
+            /**Sending empty message with static int value from MainActivity
              * to update UI ( 'Connecting...' ).
-             *
              * @see com.example.turnmeoff.MainActivity.CONNECTING
              */
             mHandler.sendEmptyMessageDelayed(map.CONNECTING, 1000);
-
-            /**
-             * Here the socket is created with hardcoded port.
+            /** Here the socket is created with hardcoded port.
              * Also the port is given in IpGetter class.
-             *
              * @see com.example.turnmeoff.IpGetter
              */
             Socket socket = new Socket(serverAddress, 57349);
 
             try {
-
                 // Create PrintWriter object for sending messages to server.
                 out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-
                 //Create BufferedReader object for receiving messages from server.
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
                 Log.d(TAG, "In/Out created");
-
                 //Sending message with command specified by AsyncTask
                 this.sendMessage(command);
-
-                //
                 mHandler.sendEmptyMessageDelayed(map.SENDING, 2000);
-
                 //Listen for the incoming messages while mRun = true
                 while (mRun) {
                     incomingMessage = in.readLine();
                     if (incomingMessage != null && listener != null) {
-
-                        /**
-                         * Incoming message is passed to MessageCallback object.
+                        /**Incoming message is passed to MessageCallback object.
                          * Next it is retrieved by AsyncTask and passed to onPublishProgress method.
-                         *
                          */
                         listener.callbackMessageReceiver(incomingMessage);
-
                     }
                     incomingMessage = null;
-
                 }
-
                 Log.d(TAG, "Received Message: " + incomingMessage);
-
             } catch (Exception e) {
-
                 Log.d(TAG, "Error on streamers", e);
                 mHandler.sendEmptyMessageDelayed(map.ERROR, 2000);
-
             } finally {
-
                 out.flush();
                 out.close();
                 in.close();
@@ -140,23 +111,16 @@ public class TCPClient {
                 mHandler.sendEmptyMessageDelayed(map.SENT, 3000);
                 Log.d(TAG, "Socket Closed");
             }
-
         } catch (Exception e) {
-
             Log.d(TAG, "Error on socket", e);
             mHandler.sendEmptyMessageDelayed(map.ERROR, 2000);
-
         }
 
     }
-
-    /**
-     * Callback Interface for sending received messages to 'onPublishProgress' method in AsyncTask.
+    /**Callback Interface for sending received messages to 'onPublishProgress' method in AsyncTask.
      */
     public interface MessageCallback {
-        /**
-         * Method overriden in AsyncTask 'doInBackground' method while creating the TCPClient object.
-         *
+        /**Method overriden in AsyncTask 'doInBackground' method while creating the TCPClient object.
          * @param message Received message from server app.
          */
         public void callbackMessageReceiver(String message);
