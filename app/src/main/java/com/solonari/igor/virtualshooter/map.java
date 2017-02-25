44f25219ead931e15ca46e86382032e134b1ba85
igorriok <i.solonari@gmail.com>
2017-02-25 15:39:57 +0200

    ship name

--- app/src/main/java/com/solonari/igor/virtualshooter/map.java
+++ app/src/main/java/com/solonari/igor/virtualshooter/map.java
@@ -239,7 +239,7 @@ public class map extends AppCompatActivity implements
 		if (result.isSuccess()) {
 		    // Signed in successfully, show authenticated UI.
 		    idToken = result.getSignInAccount().getIdToken();
-            	    Log.d(TAG, "got token: " + idToken);
+            Log.d(TAG, "got token: " + idToken);
 		    Singleton.getInstance().setString(idToken);
 		} else {
 		    // Signed out, show unauthenticated UI.
