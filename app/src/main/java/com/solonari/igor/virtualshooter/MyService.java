64b60f2013873fb1f9d5b48f6ee6430db99fd01c
igorriok <i.solonari@gmail.com>
2017-03-29 15:55:56 +0300

    service

--- app/src/main/java/com/solonari/igor/virtualshooter/MyService.java
+++ app/src/main/java/com/solonari/igor/virtualshooter/MyService.java
@@ -5,12 +5,35 @@ import android.content.Intent;
 import android.os.IBinder;
 
 public class MyService extends Service {
+    
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+    private static final String TAG = "TCPClient";
+    private final String ipNumber = "178.168.41.217";
+    private Handler mHandler;
+    private ChatManager chat;
+    SocketAddress sockaddr;
+    Socket socket;
+    
     public MyService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mMessenger.getBinder();
     }
+    
+    //Handler of incoming messages from clients.
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SAY_HELLO:
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
 }
