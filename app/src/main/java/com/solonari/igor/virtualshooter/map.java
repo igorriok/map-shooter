c3e191b78e88db0816004602d6b5538675ebe423
igor <i.solonari@gmail.com>
2017-05-30 21:05:49 +0300

    toasts

--- app/src/main/java/com/solonari/igor/virtualshooter/map.java
+++ app/src/main/java/com/solonari/igor/virtualshooter/map.java
@@ -188,13 +188,6 @@ public class map extends AppCompatActivity implements
             @Override
             public void onClick(View view) {
                 showMenu(settingsMenu);
-                settingsMenu.setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
             }
         });
     }
